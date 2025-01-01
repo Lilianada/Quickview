0a410305b1b184cc82a5554bb38cda9096dbc9c9
Bridget Amana <amanabridget03@gmail.com>
2025-01-01 16:50:01 +0100

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,125 @@
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { navSections, desSections } from "./data";
+import ThemeToggleButton from "./ThemeToggleButton";
+import { useTheme } from "../context/ThemeContext";
+
+const Header = () => {
+  const [isHeaderVisible, setIsHeaderVisible] = useState(false);
+  const [activeSection, setActiveSection] = useState("about");
+  const { theme } = useTheme();
+  const [activeDesSection, setActiveDesSection] = useState("about");
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "-50% 0px",
+      threshold: 0,
+    };
+
+    const callback = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          if (navSections.some(section => section.id === entry.target.id)) {
+            setActiveSection(entry.target.id);
+            setIsHeaderVisible(entry.target.id !== "about");
+          }
+          if (desSections.some(section => section.id === entry.target.id)) {
+            setActiveDesSection(entry.target.id);
+          }
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+
+    [...navSections, ...desSections].forEach((section) => {
+      const element = document.getElementById(section.id);
+      if (element) observer.observe(element);
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  const scrollToSection = (sectionId) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+      setActiveDesSection(sectionId);
+    }
+  };
+
+  return (
+    <div>
+      <nav className="lg:hidden flex justify-between items-center mb-16">
+        <AnimatePresence mode="wait">
+          {isHeaderVisible && (
+            <motion.div
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.3 }}
+              className={`fixed top-0 left-0 right-0 z-50 backdrop-blur-md p-4`}
+              style={{ backgroundColor: `${theme.background}33` }}
+            >
+              <motion.h1
+                className={`text-xl font-semibold text-center`}
+                style={{ color: theme.primaryText }}
+                key={activeSection}
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                transition={{ duration: 0.3 }}
+              >
+                {capitalizeFirstLetter(activeSection)}
+              </motion.h1>
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <div className="text-[36px] leading-[44px] font-nerko">LA</div>
+      </nav>
+
+      <div className="hidden lg:grid grid-rows-3">
+        <div
+          className={`text-[48px] leading-[44px] font-nerko `}
+          style={{ color: theme.primaryText }}
+        >
+          LA
+        </div>
+        <nav className="space-y-4">
+          {desSections.map((section, index) => (
+            <div
+              key={section.id}
+              className={`cursor-pointer flex items-center group
+                  ${
+                    activeDesSection === section.id
+                      ? "opacity-100"
+                      : "opacity-30 hover:opacity-60"
+                  }`}
+              style={{ color: theme.primaryText }}
+              onClick={() => scrollToSection(section.id)}
+            >
+              <span className="text-sm">0{index + 1}</span>
+              <span className="mx-2 pb-2 inline-block transition-all duration-300">
+                <span
+                  className={`inline-block h-[1.5px] bg-current transition-all duration-300 ${
+                    activeDesSection === section.id ? "w-12" : "w-8"
+                  }`}
+                />
+              </span>
+              <span className="font-medium">{section.label}</span>
+            </div>
+          ))}
+        </nav>
+
+        <ThemeToggleButton />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/components/data.js
+++ src/components/data.js
@@ -99,80 +99,80 @@ export const desSections = [
 
 export const themeConfig = {
     default: {
-        background: '[#242424]',
-        primaryText: '[#FFFFFF]',
-        secondaryText: '[rgba(209, 209, 214, 1)]',
-        accent: '[#F5BF6A]',
-        card: '[#2E2E2E]'
+        background: '#242424',
+        primaryText: '#FFFFFF',
+        secondaryText: 'rgba(209, 209, 214, 1)',
+        accent: '#F5BF6A',
+        card: '#2E2E2E'
     },
     darkCyan: {
-        background: '[#051a1c]',
-        primaryText: '[#c3ebf1]',
-        secondaryText: '[rgba(183, 212, 245, 0.4)]',
-        accent: '[#164E63]',
-        card: '[#CFFAFE]'
+        background: '#051a1c',
+        primaryText: '#c3ebf1',
+        secondaryText: 'rgba(183, 212, 245, 0.4)',
+        accent: '#164E63',
+        card: '#CFFAFE'
     },
     oceanDusk: {
-        background: '[#182938]',
-        primaryText: '[#b7d4f5]',
-        secondaryText: '[rgba(183, 212, 245, 0.01)]',
-        accent: '[rgba(242, 195, 164, 0.12)]',
-        card: '[#182938]'
+        background: '#182938',
+        primaryText: '#b7d4f5',
+        secondaryText: 'rgba(183, 212, 245, 0.01)',
+        accent: 'rgba(242, 195, 164, 0.12)',
+        card: '#182938'
     },
     twilightLavender: {
-        background: '[#2e3248]',
-        primaryText: '[#cfbcec]',
-        secondaryText: '[rgba(154, 109, 223, 0.5)]',
-        accent: '[#673AB7]',
-        card: '[#3F3B4D]'
+        background: '#2e3248',
+        primaryText: '#cfbcec',
+        secondaryText: 'rgba(154, 109, 223, 0.5)',
+        accent: '#673AB7',
+        card: '#3F3B4D'
     },
     pastelRose: {
-        background: '[#40364f]',
-        primaryText: '[#f8a6d3]',
-        secondaryText: '[rgba(211, 153, 186, 0.6)]',
-        accent: '[#F17EB1]',
-        card: '[#574F6D]'
+        background: '#40364f',
+        primaryText: '#f8a6d3',
+        secondaryText: 'rgba(211, 153, 186, 0.6)',
+        accent: '#F17EB1',
+        card: '#574F6D'
     },
     royalBlush: {
-        background: '[#5543c5]',
-        primaryText: '[#ffb1b1]',
-        secondaryText: '[rgba(255, 177, 177, 0.3)]',
-        accent: '[#FF8080]',
-        card: '[#6E57CF]'
+        background: '#5543c5',
+        primaryText: '#ffb1b1',
+        secondaryText: 'rgba(255, 177, 177, 0.3)',
+        accent: '#FF8080',
+        card: '#6E57CF'
     },
     peachCream: {
-        background: '[#ffd2b4]',
-        primaryText: '[#e94f0e]',
-        secondaryText: '[rgba(233, 79, 14, 0.15)]',
-        accent: '[#FF8C42]',
-        card: '[#FFE6D0]'
+        background: '#ffd2b4',
+        primaryText: '#e94f0e',
+        secondaryText: 'rgba(233, 79, 14, 0.15)',
+        accent: '#FF8C42',
+        card: '#FFE6D0'
     },
     vintageBeige: {
-        background: '[#f3e6cf]',
-        primaryText: '[#774e34]',
-        secondaryText: '[rgba(119, 78, 52, 0.25)]',
-        accent: '[#D9A066]',
-        card: '[#F6EDD6]'
+        background: '#f3e6cf',
+        primaryText: '#774e34',
+        secondaryText: 'rgba(119, 78, 52, 0.25)',
+        accent: '#D9A066',
+        card: '#F6EDD6'
     },
     iceBlue: {
-        background: '[#deebee]',
-        primaryText: '[#1e282a]',
-        secondaryText: '[rgba(46, 62, 64, 0.5)]',
-        accent: '[#4A7078]',
-        card: '[#E3F2F5]'
+        background: '#deebee',
+        primaryText: '#1e282a',
+        secondaryText: 'rgba(46, 62, 64, 0.5)',
+        accent: '#4A7078',
+        card: '#E3F2F5'
     },
     frostedGlass: {
-        background: '[#f2fbfd]',
-        primaryText: '[#12191a]',
-        secondaryText: '[rgba(18, 25, 26, 0.3)]',
-        accent: '[#5D7F81]',
-        card: '[#F0FCFE]'
+        background: '#f2fbfd',
+        primaryText: '#12191a',
+        secondaryText: 'rgba(18, 25, 26, 0.3)',
+        accent: '#5D7F81',
+        card: '#F0FCFE'
     },
     light: {
-        background: '[#ffffff]',
-        primaryText: '[#000000]',
-        secondaryText: '[rgba(82, 82, 91, 0.5)]',
-        accent: '[#D94A1F]',
-        card: '[#F8F8F8]'
+        background: '#ffffff',
+        primaryText: '#000000',
+        secondaryText: 'rgba(82, 82, 91, 0.5)',
+        accent: '#D94A1F',
+        card: '#F8F8F8'
     },
 };
