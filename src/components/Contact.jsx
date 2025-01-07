804f70e7cff3062294a9c6be815687a2de59a9cc
Bridget Amana <amanabridget03@gmail.com>
2025-01-08 00:07:55 +0100

    Update Contact layout and add social media links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useTheme } from "../context/ThemeContext";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 const Contact = () => {
   const { theme } = useTheme();
   return (
     <section className="hidden lg:flex flex-col">
-      <div id="desktop-contact" className="my-12">
+      <div id="desktop-contact" className="mt-12">
         <div className="">
           <h2
             className={`text-[70px] font-[900] leading-[70px] w-1/3 mb-24`}
@@ -14,39 +15,81 @@ const Contact = () => {
             Contact
           </h2>
         </div>
-        <div className="space-y-3">
+        <div className="space-y-5">
           <span className="flex items-center gap-2">
-            <p className="text-sm shrink-0">Email</p>
+            <p className="shrink-0">Email</p>
             <span className="flex-grow">
               <span
-                className="block h-[1.5px] w-full opacity-70"
+                className="block h-[1px] w-full opacity-30"
                 style={{ backgroundColor: theme.primaryText }}
               />
             </span>
-            <p className="text-sm shrink-0">Lilianokeke.ca@gmail.com</p>
+            <span className="flex items-center gap-1.5 text-blue-400">
+              <a href="mailto:Lilianokeke.ca@gmail.com" className="shrink-0">
+                Lilianokeke.ca@gmail.com
+              </a>
+              <MdKeyboardArrowRight />
+            </span>
           </span>
 
           <span className="flex items-center gap-2">
-            <p className="text-sm shrink-0">Email</p>
+            <p className="shrink-0">Twitter</p>
             <span className="flex-grow">
               <span
-                className="block h-[1.5px] w-full opacity-70"
+                className="block h-[1px] w-full opacity-30"
                 style={{ backgroundColor: theme.primaryText }}
               />
             </span>
-            <p className="text-sm shrink-0">Lilianokeke.ca@gmail.com</p>
+            <span className="flex items-center gap-1.5 text-blue-400">
+              <a href="http:" className="shrink-0">
+                @Lilianokeke
+              </a>
+              <MdKeyboardArrowRight />
+            </span>
           </span>
 
           <span className="flex items-center gap-2">
-            <p className="text-sm shrink-0 opacity-70">Email</p>
+            <p className="shrink-0">LinkedIn</p>
             <span className="flex-grow">
               <span
-                className="block h-[1.5px] w-full"
+                className="block h-[1px] w-full opacity-30"
                 style={{ backgroundColor: theme.primaryText }}
               />
             </span>
-            <p className="text-sm shrink-0">Lilianokeke.ca@gmail.com</p>
+            <span className="flex items-center gap-1.5 text-blue-400">
+              <a href="http:" className="shrink-0">
+                @Lilianokeke
+              </a>
+              <MdKeyboardArrowRight />
+            </span>
           </span>
+
+          <span className="flex items-center gap-2">
+            <p className="shrink-0">Github</p>
+            <span className="flex-grow">
+              <span
+                className="block h-[1px] w-full opacity-30"
+                style={{ backgroundColor: theme.primaryText }}
+              />
+            </span>
+            <span className="flex items-center gap-1.5 text-blue-400">
+              <a href="http:" className="shrink-0">
+                @Lilianokeke
+              </a>
+              <MdKeyboardArrowRight />
+            </span>
+          </span>
+        </div>
+
+        <div className="mt-16">
+          <span className="">
+            <span
+              className="block h-[1px] w-full opacity-30"
+              style={{ backgroundColor: theme.primaryText }}
+            />
+          </span>
+
+          <p className="mt-4">© 2023 - Lilian Okeke.</p>
         </div>
       </div>
     </section>
