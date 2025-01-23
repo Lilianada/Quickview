1c422f89ce61aaceb8d880c15c06f48995e8a855
Lilianada <lilibest30.lb@gmail.com>
2025-01-23 10:41:13 +0100

    added  h-8 w-28  for anchor tag in book call button

--- src/components/PurchaseButton.jsx
+++ src/components/PurchaseButton.jsx
@@ -4,7 +4,7 @@ export default function PurchaseButton() {
   return (
     <div className="fixed bottom-20 right-4 lg:left-4">
     <button className='bg-gray-50 h-8 w-28  rounded-lg' >
-       <a href="https://cal.com/lilian-okeke-s3usyz/30min" target='_blank' rel='noreferrer' className="text-sm text-gray-900 font-semibold">Book a call</a>
+       <a href="https://cal.com/lilian-okeke-s3usyz/30min" target='_blank' rel='noreferrer' className="text-sm text-gray-900 h-8 w-28 font-semibold">Book a call</a>
     </button>
     </div>
   )
